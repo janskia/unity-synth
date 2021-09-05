129c2f41c603c6e55ea5820ae7fc2a588f9d79cc
janskia <janski.adam@gmail.com>
2021-09-05 17:38:28 +0200

    curve sampler

--- Assets/Scripts/Sampler.cs
+++ Assets/Scripts/Sampler.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class Sampler : MonoBehaviour
 {
     public int samplesArraySize = 256;
-    
+    public AnimationCurve curve;
+
     private float[] samples;
 
     private void Awake()
     {
         samples = new float[samplesArraySize];
+    }
+
+    private void FixedUpdate()
+    {
         for (int i = 0; i < samples.Length; i++)
         {
-            samples[i] = Mathf.Sin((float)i / samples.Length * 2 * Mathf.PI);
+            //  samples[i] = Mathf.Sin((float)i / samples.Length * 2 * Mathf.PI);
+            samples[i] = curve.Evaluate((float)i / samples.Length);
         }
     }
 
