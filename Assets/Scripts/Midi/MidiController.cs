bc66eb991177aa0b50a5cef4d8503acd3251a423
janskia <janski.adam@gmail.com>
2021-09-15 22:17:21 +0200

    removed midiJack added minis

---
+++ Assets/Scripts/Midi/MidiController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MidiController : MonoBehaviour
+{
+    void Start()
+    {
+        InputSystem.onDeviceChange += (device, change) =>
+        {
+            if (change != InputDeviceChange.Added) return;
+
+            var midiDevice = device as Minis.MidiDevice;
+            if (midiDevice == null) return;
+
+            midiDevice.onWillNoteOn += (note, velocity) => {
+                // Note that you can't use note.velocity because the state
+                // hasn't been updated yet (as this is "will" event). The note
+                // object is only useful to specify the target note (note
+                // number, channel number, device name, etc.) Use the velocity
+                // argument as an input note velocity.
+                Debug.Log(string.Format(
+                    "Note On #{0} ({1}) vel:{2:0.00} ch:{3} dev:'{4}'",
+                    note.noteNumber,
+                    note.shortDisplayName,
+                    velocity,
+                    (note.device as Minis.MidiDevice)?.channel,
+                    note.device.description.product
+                ));
+            };
+
+            midiDevice.onWillNoteOff += (note) => {
+                Debug.Log(string.Format(
+                    "Note Off #{0} ({1}) ch:{2} dev:'{3}'",
+                    note.noteNumber,
+                    note.shortDisplayName,
+                    (note.device as Minis.MidiDevice)?.channel,
+                    note.device.description.product
+                ));
+            };
+        };
+    }
+}
