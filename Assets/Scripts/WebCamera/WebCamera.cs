30684a785360aa3735d24365829154cfb0734d6f
janskia <janski.adam@gmail.com>
2025-01-19 10:20:34 +0100

    Web camera input.

---
+++ Assets/Scripts/WebCamera/WebCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class WebCamera : MonoBehaviour
+{
+
+    private void Start()
+    {
+        InitializeCamera();
+    }
+
+    private void InitializeCamera()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+        Debug.Log(webcamTexture.deviceName);
+    }
+}
